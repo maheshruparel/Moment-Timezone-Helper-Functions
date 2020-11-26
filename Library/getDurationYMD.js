898d126e9626e25f9e1b1f0618f1cd8f00d2b213
Mahesh Ruparel <maheshruparel@gmail.com>
2020-11-26 15:12:15 +0530

    Update getDurationYMD.js

--- Library/getDurationYMD.js
+++ Library/getDurationYMD.js
@@ -1,1 +1,16 @@
+const getDurationYMD = (smallDate, bigDate, includeBothDates) => {
+  smallDate = moment(smallDate);
+  bigDate = moment(bigDate);
+
+  if (includeBothDates) {
+    bigDate.add(1, 'day');
+  }
+  let years = bigDate.diff(smallDate, 'years');
+  smallDate.add(years, 'years');
+  let months = bigDate.diff(smallDate, 'months');
+  smallDate.add(months, 'months');
+  let days = bigDate.diff(smallDate, 'days');
+  smallDate.add(days, 'days');
+  return { years, months, days };
+};
 
